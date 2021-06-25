0bb9174076bfb8486038955c3bf0ebe6da2a1ce3
bruno7y62 <bruno7y62@gmail.com>
2021-06-25 16:41:28 -0500

    Added MainContainer page and styles

--- hana_shop_client/src/components/main/MainContainer.jsx
+++ hana_shop_client/src/components/main/MainContainer.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import Hana_face from '../../assets/images/Hana_face.png';
+import CustomizableButton from '../utilities/CustomizableButton';
+import '../../assets/stylesheets/main_container.scss';
+
 
 const MainContiner = () => {
     return(
-        <p>Soy el Main</p>
+        <div className="main-container">
+            <div className="left">
+                <div className="text-container">
+                    <p className="main-text">
+                        Welcome to Hana, on this website you will find various products that may be of interest to you.
+                    </p> 
+                </div>
+                <div className="button-container">
+                    <Link className="styled-link" to="/store">
+                        <CustomizableButton text="Go to store" stylesheet="button-main"/>
+                    </Link>
+                </div>         
+                <div className="link-container">
+                    <Link className="styled-link" to="/about"><h1 className="link">About Us</h1></Link>
+                    <Link className="styled-link" to="/contact"><h1 className="link">Contact</h1></Link>
+                </div>
+            </div>
+            <div className="right">
+                <img className="hana" src={Hana_face} alt="Hana_face" />
+            </div>
+        </div>
     );
 }
 
