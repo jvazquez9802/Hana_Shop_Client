09649e4d20d94a42ad51ab9ca61557aeed765c90
= <=>
2021-06-29 22:17:46 -0500

    [ADDED] Series Routes/Controller/Model

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-itemsController = require('../controllers/controller');
-
-exports.appRoute = (router) => {
-    router.get("/menu", itemsController, (req,res) => {
-        res.json(itemsController.getAll())
-    });
-};
